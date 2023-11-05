6059b82cbee66fe534c59ca11df1249f6f72ac99
glesiosantos <glesioss@gmail.com>
2023-11-05 12:06:36 -0300

    feat: ensure SignUpController return 400 when password confirmatio is fail

--- src/presentation/controllers/signup_controller.ts
+++ src/presentation/controllers/signup_controller.ts
@@ -1,3 +1,4 @@
+import { InvalidParamError } from '../errors/invalid_param_error'
 import { MissingParamError } from '../errors/missing_param_error'
 import { badRequest } from '../helpers/http_helper'
 import { type Controller } from '../protocols/controller'
@@ -11,6 +12,10 @@ export class SignUpController implements Controller {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+
+    if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      return badRequest(new InvalidParamError('passwordConfirmation'))
     }
   }
 }
