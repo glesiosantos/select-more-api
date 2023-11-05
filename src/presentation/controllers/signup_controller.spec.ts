3ab51cdb387b02c4550ab7aeecd387b3956dc757
glesiosantos <glesioss@gmail.com>
2023-11-05 11:43:14 -0300

    test: move sut creation to a factory helper method

--- src/presentation/controllers/signup_controller.spec.ts
+++ src/presentation/controllers/signup_controller.spec.ts
@@ -1,8 +1,10 @@
 import { SignUpController } from './signup_controller'
 
+const makeSut = (): any => new SignUpController()
+
 describe('Sign Up Controller', () => {
   it('should return 400 when name is not provided', async () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_mail@email.com.br',
@@ -15,7 +17,7 @@ describe('Sign Up Controller', () => {
   })
 
   it('should return 400 when email is not provided', async () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
