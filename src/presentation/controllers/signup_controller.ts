1a59af02f656afb4291c868e9cc96c4a346e5f53
Glêsio Santos <116793448+glesiosantos@users.noreply.github.com>
2023-11-06 09:18:51 -0300

    Sign Up Controller / Presentation (#1)
    * chore: add husky and lint staged
    
    * chore: setting jest and scripts
    
    * feat: ensure SignUpController return 400 when name is not provided
    
    * test: ensure SignUpController return 400 when name is not provided
    
    * feat: ensure SignUpController return 400 when email is not provided
    
    * test: ensure SignUpController return 400 when email is not provided
    
    * test: move sut creation to a factory helper method
    
    * refactor: implement controller protocols
    
    * refactor: created error missingParam
    
    * refactor: implement to protocols
    
    * refactor: move return bad request a to helper method in http helper file
    
    * feat: ensure SignUpController return 400 when password is not provided
    
    * test: ensure SignUpController return 400 when password is not provided
    
    * feat: ensure SignUpController return 400 when password confirmation is not provided
    
    * test: ensure SignUpController return 400 when password confirmation is not provided
    
    * feat: ensure SignUpController return 400 when password confirmatio is fail
    
    * test: ensure SignUpController return 400 when password confirmatio is fail
    
    * feat: ensure SignUpController calls EmailValidator with correct values
    
    * test: ensure SignUpController calls EmailValidator with correct value
    
    * feat: ensure SignUpController return 400 when EmailValidator return false
    
    * test: ensure SignUpController return 400 when EmailValidator return false
    
    * feat: ensure SignUpController return 500 when EmailValidator throws
    
    * test: ensure SignUpController return 500 when EmailValidator throws
    
    * chore: add coverage in .gitignore

---
+++ src/presentation/controllers/signup_controller.ts
@@ -0,0 +1,31 @@
+import { EmailValidator } from '../../validators/email_validator'
+import { InvalidParamError } from '../errors/invalid_param_error'
+import { MissingParamError } from '../errors/missing_param_error'
+import { badRequest } from '../helpers/http_helper'
+import { type Controller } from '../protocols/controller'
+import { HttpRequest, HttpResponse } from '../protocols/http'
+
+export class SignUpController implements Controller {
+  constructor(private readonly emailValidator: EmailValidator) { }
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+    } catch (error) {
+      return { statusCode: 500, body: new Error('Internal Server Error') }
+    }
+  }
+}
