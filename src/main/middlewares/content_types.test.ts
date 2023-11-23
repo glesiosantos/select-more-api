ee1d0df552e0b0bc24f63733ad5a7bd71171bf75
glesiosantos <glesioss@gmail.com>
2023-11-23 09:52:14 -0300

    test: ensure return xml content type when forced

--- src/main/middlewares/content_types.test.ts
+++ src/main/middlewares/content_types.test.ts
@@ -9,4 +9,15 @@ describe('Content Types Middlewares Test', () => {
       .get('/content_types')
       .expect('content-type', /json/)
   })
+
+  it('should return xml content_type when forced', async () => {
+    app.get('/content_type_xml', (req, res) => {
+      res.type('xml')
+      return res.send('')
+    })
+
+    await request(app)
+      .get('/content_type_xml')
+      .expect('content-type', /xml/)
+  })
 })
